b3871939c2ad4e9bfb8b3eaa66ca2b06b9c951cd
Udit Karode <udit.karode@gmail.com>
2020-03-20 01:54:04 +0530

    actions: setup CI

---
+++ pushbuild.sh
@@ -0,0 +1,15 @@
+if [ -d build/outputs/apk ]; then
+        if [ -n "$(ls -A build/outputs/apk)" ]; then
+          mkdir ~/tmp
+          cp build/outputs/apk/*.apk ~/tmp
+          git checkout -b builds
+          rm *.apk -f
+          cp ~/tmp/* .
+          git add .
+          git config --global user.name 'Bob The Builder'
+          git config --global user.email 'bob@the.builder'
+          git remote set-url origin "https://x-access-token:${GH_TOKEN}@github.com/${GH_REPO}"
+          git commit -m "bot: build installables <$(TZ=Asia/Kolkata date +'%Y%m%d %T')>"
+          git push -u origin builds
+	fi
+fi

5a7dcceb499bf94804731018e5c0cbb1a99ee8f4
Udit Karode <udit.karode@gmail.com>
2020-03-21 20:42:20 +0530

    actions: build arm64-v8a and armeabi-v7a
    * really inefficient but done in a hurry, and GitHub Actions is free.

--- pushbuild.sh
+++ pushbuild.sh
@@ -1,17 +1,19 @@
-echo "pushing build..."
 if [ -d app/build/outputs/apk/debug ]; then
         if [ -n "$(ls -A app/build/outputs/apk/debug)" ]; then
           mkdir ~/tmp
-          cp app/build/outputs/apk/debug/*.apk ~/tmp/AbleMusic-arm64.apk
-          git clone https://github.com/uditkarode/AbleMusicPlayer -b builds
+          mv app/build/outputs/apk/debug/*.apk ~/tmp/AbleMusic-${1}.apk
+          rm -rf app/build/outputs/apk
+	  git clone https://github.com/uditkarode/AbleMusicPlayer -b builds
 	  cd AbleMusicPlayer 
-          rm *.apk -f
-          cp ~/tmp/* .
+          mv ~/tmp/* .
           git add .
           git config --global user.name 'Bob The Builder'
           git config --global user.email 'bob@the.builder'
           git remote set-url origin "https://x-access-token:${GH_TOKEN}@github.com/${GH_REPO}"
-          git commit -m "bot: build installables <$(TZ=Asia/Kolkata date +'%Y%m%d %T')>"
+          git commit -m "bot: build arch: ${1} <$(TZ=Asia/Kolkata date +'%Y%m%d %T')>"
           git push -u origin builds
+	  cd ..
+	  rm -rf AbleMusicPlayer
+	  rm -rf ~/tmp
 	fi
 fi

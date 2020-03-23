978c00ef3f9663a486fd61839c66e66835007291
Udit Karode <udit.karode@gmail.com>
2020-03-23 23:38:04 +0530

    pushbuild: append short HEAD to build filenames

--- pushbuild.sh
+++ pushbuild.sh
@@ -1,7 +1,7 @@
 if [ -d app/build/outputs/apk/release ]; then
         if [ -n "$(ls -A app/build/outputs/apk/release)" ]; then
           mkdir ~/tmp
-          mv app/build/outputs/apk/release/*.apk ~/tmp/AbleMusic-${1}.apk
+	  mv app/build/outputs/apk/release/*.apk ~/tmp/AbleMusic-${1}-$(echo $GITHUB_SHA | cut -c1-6).apk
           rm -rf app/build/outputs/apk
 	  git clone https://github.com/uditkarode/AbleMusicPlayer -b builds
 	  cd AbleMusicPlayer 

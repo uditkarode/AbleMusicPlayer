30792579af091c2d6271bdf9b53a858b9d5fa739
Udit Karode <udit.karode@gmail.com>
2020-03-25 00:12:16 +0530

    activities: Welcome: cleanup

--- app/src/main/java/io/github/uditkarode/able/activities/Welcome.kt
+++ app/src/main/java/io/github/uditkarode/able/activities/Welcome.kt
@@ -19,14 +19,10 @@
 package io.github.uditkarode.able.activities
 
 import android.Manifest
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import io.github.dreierf.materialintroscreen.MaterialIntroActivity
-import io.github.dreierf.materialintroscreen.MessageButtonBehaviour
 import io.github.dreierf.materialintroscreen.SlideFragmentBuilder
 import io.github.uditkarode.able.R
-
 
 class Welcome: MaterialIntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +40,7 @@ class Welcome: MaterialIntroActivity() {
                 .image(io.github.dreierf.materialintroscreen.R.drawable.ic_next)
                 .title("Welcome to AbleMusic!")
                 .description("The storage permission is required to proceed. This is needed" +
-                        " so that Able can download songs to your storage.")
+                        " for Able to download songs to your storage.")
                 .build()
         )
     }

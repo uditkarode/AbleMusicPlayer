fc0d89e6fcf7ac55e860ecca8f64b0c1777fe2ef
Udit Karode <udit.karode@gmail.com>
2020-03-30 15:20:35 +0530

    activities: Player: add visual sublisting indications

--- app/src/main/java/io/github/uditkarode/able/models/Song.kt
+++ app/src/main/java/io/github/uditkarode/able/models/Song.kt
@@ -19,4 +19,4 @@
 package io.github.uditkarode.able.models
 
 class Song(val name: String, var artist: String="", var youtubeLink: String = "",
-           var filePath: String = "")
+           var filePath: String = "", var placeholder: Boolean = false)

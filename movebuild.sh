5cacbb176a1b8253357c35c1a3ab21877805273a
Udit Karode <udit.karode@gmail.com>
2020-06-03 01:47:07 +0530

    movebuild: properly append commit hash

--- movebuild.sh
+++ movebuild.sh
@@ -1,8 +1,8 @@
 if [ -d app/build/outputs/apk/release ]; then
         if [ -n "$(ls -A app/build/outputs/apk/release)" ]; then
-          cp app/build/outputs/apk/release/*.apk GitBuilds/AbleMusic-${1}-$(echo $GITHUB_SHA | cut -c1-8).apk
+          cp app/build/outputs/apk/release/*.apk GitBuilds/AbleMusic-${1}-$(echo $TRIGGERING_SHA | cut -c1-8).apk
           cd GitBuilds
-          curl --progress-bar -F document=@"AbleMusic-${1}-$(echo $GITHUB_SHA | cut -c1-8).apk" "https://api.telegram.org/bot${TG_BOT_KEY}/sendDocument" \
+          curl --progress-bar -F document=@"AbleMusic-${1}-$(echo $TRIGGERING_SHA | cut -c1-8).apk" "https://api.telegram.org/bot${TG_BOT_KEY}/sendDocument" \
             -F chat_id="-1001415196670"  \
             -F "disable_web_page_preview=true" \
             -F "parse_mode=Markdown" \

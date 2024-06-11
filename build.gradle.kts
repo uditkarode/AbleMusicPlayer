75f13bc19e7f32a865fa63dc219e3ff994a4c955
MadFlasheroo7 <jayeshseth45@gmail.com>
2024-06-11 13:21:08 +0530

    changes: clean gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,6 @@
-//buildscript {
-////    ext.kotlin_version = "1.7.10"
-//    repositories {
-//        google()
-//        mavenCentral()
-//    }
-//    dependencies {
-//        classpath (libs.android.gradlePlugin)
-//        classpath (libs.kotlin.gradlePlugin)
-//    }
-//}
-
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlin.jvm) apply false
 }
-
-
-//allprojects {
-//    repositories {
-//        google()
-//        jcenter()
-//        mavenCentral()
-//        maven { url 'https://jitpack.io' }
-//        flatDir {
-//            dirs 'src/main/libs'
-//        }
-//    }
-//}
-
-//task clean(type: Delete) {
-//    delete rootProject.buildDir
-//}
